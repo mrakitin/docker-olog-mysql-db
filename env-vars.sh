575035a6225a736031d51668d1680e3b739ecd3e
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-11-26 23:04:50 -0500

    Remove the network specification

--- env-vars.sh
+++ env-vars.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DOCKER_MANTAINER_NAME=lnlscon
+DOCKER_MANTAINER_NAME=mrakitin
 DOCKER_NAME=olog-mysql-db
 DOCKER_RUN_NAME=olog-mysql-db
 DOCKER_TAG=latest
 
-MYSQL_USER=lnls_olog_user
-MYSQL_PASSWORD=controle
+MYSQL_USER=olog_user
+MYSQL_PASSWORD=password
 MYSQL_DATABASE=olog
 MYSQL_PORT=3306
